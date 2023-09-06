7a5cc3ce388460bd92d461ad5aa42918603498c2
David Peláez <davidpelaez.ing@gmail.com>
2023-09-06 08:06:15 -0500

    creacion de trait Helper

---
+++ app/Traits/HelperTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait HelperTrait
+{
+
+    /**
+     * Asigna el campo user_id a un array, en caso de no enviar id se asigna el del usuario autenticado
+     * @param array $data
+     * @param int $user_id <optional>
+     * @return array
+     * @author David Peláez
+     */
+    public function agregarCamporUserId($data, $user_id = null): array
+    {
+        $data['user_id'] = $user_id ?? Auth::id();
+        return $data;
+    }
+}
