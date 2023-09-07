ecab46553e020ed97af5c638d43cea74ca3e502a
David Peláez <davidpelaez.ing@gmail.com>
2023-09-07 14:19:45 -0500

    add: trait para el trato de imagenes

---
+++ app/Traits/ImagenTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Storage;
+
+trait ImagenTrait
+{
+
+    /**
+     * sube una imagen y retorna un unique id 
+     * @param string $ruta
+     * @param file $file
+     * @return string
+     * @author David Peláez
+     */
+    public function subirImagen($ruta, $file): string
+    {
+        $nombre_unique = uniqid() . '.' . $file->getClientOriginalExtension();
+        return Storage::putFileAs($ruta, $file, $nombre_unique);
+    }
+
+    /**
+     * elimina una imagen de la base de datos
+     * @param string|Array $rutas
+     * @return boolean
+     * @author David Peláez
+     */
+    public function eliminarImagen($rutas): bool
+    {
+        return Storage::delete($rutas);
+    }
+}
